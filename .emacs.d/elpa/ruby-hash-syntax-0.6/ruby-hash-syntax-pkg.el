1e21a7c18677d7fa536db103440ae0cc0c47325f
Mike Nichols <mykgithub@fastmail.com>
2021-09-06 16:06:07 -0700

    Update emacs packages 2021-09-06

--- .emacs.d/elpa/ruby-hash-syntax-0.6/ruby-hash-syntax-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /Users/mike/.emacs.d/elpa/ruby-hash-syntax-0.6/ruby-hash-syntax.el  -*- no-byte-compile: t -*-
-(define-package "ruby-hash-syntax" "0.6" "Toggle ruby hash syntax between classic and 1.9 styles" 'nil :commit "89fc364a837d7a78ecce34380f09c073a83e30e0" :keywords '("languages") :authors '(("Steve Purcell" . "steve@sanityinc.com")) :maintainer '("Steve Purcell" . "steve@sanityinc.com") :url "https://github.com/purcell/ruby-hash-syntax")
