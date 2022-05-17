2b1e5c9cf17b7332d037f1a2f1880ae06f501609
Mike Nichols <mykgithub@fastmail.com>
2022-05-17 11:56:49 -0500

    Don't put coreutils in PATH

--- common-mac.sh
+++ common-mac.sh
@@ -1,18 +1,6 @@
 ## common-mac.sh
 
 #-------------------------------------------------------------------------------
-# coreutils
-
-if type brew &> /dev/null; then
-  coreutils_dir="$(brew --prefix coreutils)/libexec"
-
-  if [ -d "$coreutils_dir" ]; then
-    export PATH="$coreutils_dir/gnubin:$PATH"
-    export MANPATH="$coreutils_dir/gnuman:$MANPATH"
-  fi
-fi
-
-#-------------------------------------------------------------------------------
 # chrome
 
 # From https://blog.ideasynthesis.com/2018/09/24/Disable-Google-Chrome-Sign-In-and-Sync/
